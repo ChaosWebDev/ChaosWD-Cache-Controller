a6f2c093014eeccfbe31192bf47eb1c5e0792dfd
jpgerber1986 <jordangerber@gmail.com>
2023-05-25 16:12:19 -0600

    v0.1.0

---
+++ src/CacheController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Order\Controller;
+
+class CacheController
+{
+    public $dir, $file;
+
+    public function __construct(string $dir)
+    {
+        $this->dir = $dir;
+        $this->checkLogExists($this->dir);
+    }
+
+    private function checkLogExists($dir)
+    {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+        return;
+    }
+
+    private function encodeKey($key)
+    {
+        $encodedKey = md5($key);
+        $this->file = $this->dir . "/" . $encodedKey . ".tmp";
+        return;
+    }
+
+    public function load($key)
+    {
+        $this->encodeKey($key);
+        if (file_exists($this->file)) {
+            $contents = unserialize(file_get_contents($this->file));
+
+            $exp = $contents['expiration'];
+            $content = base64_decode($contents['content']);
+
+            if ($exp <= strtotime("now")) {
+                return ["false"];
+            } else {
+                return ["true", $content];
+            }
+        } else {
+            return ["false"];
+        }
+    }
+
+    public function save(string $key, $content, int $expiration = 300)
+    {
+        $this->encodeKey($key);
+        $expiration = time() + $expiration;
+
+        if (!file_exists($this->file)) {
+            fclose(fopen($this->file, "w+"));
+        }
+
+        $array = array("expiration" => $expiration, "content" => base64_encode($content));
+        $savingData = serialize($array);
+        file_put_contents($this->file, $savingData);
+        return;
+    }
+}
--- test/index.php
+++ test/index.php
@@ -1,3 +1,31 @@
 <?php
+
 require(__DIR__ . '/../vendor/autoload.php');
 
+use Order\Controller\CacheController;
+
+$cache = new CacheController(__DIR__ . "/cache");
+
+load($cache);
+
+function save($cache, $content)
+{
+    echo "SAVING!!<br><br>";
+    $cache->save("index", $content, 20);
+    return;
+}
+
+function load($cache)
+{
+    echo "LOADING<br><br>";
+    $loadedCache = $cache->load("index");
+    if ($loadedCache[0] == "true") {
+        echo "Loaded Cache<br><br>";
+        echo $loadedCache[1];
+    } else {
+        echo "Manually Get Content for Display<br><br>";
+        $content = file_get_contents("content.txt");
+        save($cache, $content);
+    }
+    return;
+}
